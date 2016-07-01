d1e2cea27ec354f98232109a935452bee079fa01
runrunliuliu <liuliu.tju@gmail.com>
2016-07-01 15:35:20 +0800

    update download method

--- download.sh
+++ download.sh
@@ -6,5 +6,8 @@ echo $day0
 python dbaction.py -a fetchindex
 python download.py $day0
 
-scp himalayas@139.129.99.51:/home/himalayas/apps2/qts/pytrade2/data/dayk/* ./data/dayk/
+# scp himalayas@139.129.99.51:/home/himalayas/apps2/qts/pytrade2/data/dayk/* ./data/dayk/
+scp himalayas@139.129.99.51:/home/himalayas/apps2/qts/pytrade2/data/dayk.tar.gz .
+tar -zxvf dayk.tar.gz
+
 scp himalayas@139.129.99.51:/home/himalayas/apps2/qts/pytrade2/data/stockinfo.csv ./data/
