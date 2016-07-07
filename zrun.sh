dd4f9cf3d1249ea7add57df55658af8b5f53a368
runrunliuliu <liuliu.tju@gmail.com>
2016-07-07 19:00:00 +0800

    fix bugs

--- zrun.sh
+++ zrun.sh
@@ -6,15 +6,14 @@ echo $py
 day0=`date +"%Y-%m-%d"`
 echo $day0
 
-###--- TRAIN ---
-$py pp_selstock.py -m 'train' -s 2016-05-06 -d $day0 -t triangle
-
 ###--- DOWNLOAD DT_BOARD
 scp himalayas@139.129.99.51:/home/himalayas/apps2/qts/pytrade2/data/dtboard.csv ./data/ 
 
 ###--- 计算龙虎榜
 $py indicator.py
 
+###--- TRAIN ---
+$py pp_selstock.py -m 'train' -s 2016-05-06 -d $day0 -t triangle
 $py pp_selstock.py -m 'stock' -s 2016-05-06 -d $day0 -t triangle
 $py pp_selstock.py -m 'stock' -s 2016-05-06 -d $day0 -t QUSHI
 
