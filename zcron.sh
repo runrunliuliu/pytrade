b4998c4b52a7f93013d41d7abebf1858c314137f
runrunliuliu <liuliu.tju@gmail.com>
2016-10-09 17:15:05 +0800

    add kline

--- zcron.sh
+++ zcron.sh
@@ -27,5 +27,6 @@ $py pp_selstock.py -m 'train' -s 2016-08-01 -d $day0 -t triangle -p day
 nohup $py pp_selstock.py -m 'stock' -s 2016-08-01 -d $day0 -t triangle 1>logs/$day0.triangle.log 2>&1 &
 nohup $py pp_selstock.py -m 'stock' -s 2016-08-01 -d $day0 -t QUSHI 1>logs/$day0.qushi.log 2>&1 &
 nohup $py pp_selstock.py -m 'stock' -s 2016-08-01 -d $day0 -t nbs 1>logs/$day0.nbs.log 2>&1 &
+nohup $py pp_selstock.py -m 'stock' -s 2016-08-01 -d $day0 -t kline 1>logs/$day0.kline.log 2>&1 &
 
 scp data/dayk/* himalayas@192.168.200.11:/data/server/nfsclient/stock/dayk/ 1>upload.log 2>&1
