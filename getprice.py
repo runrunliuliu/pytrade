531124f4be2209e7ad76e058fab5959db0099fd2
runrunliuliu <liuliu.tju@gmail.com>
2016-11-03 17:52:11 +0800

    delete files

--- getprice.py
+++
@@ -1,6 +0,0 @@
-import requests
-for line in open('data/holds.txt'):
-    url = 'http://api.quchaogu.com/stock/batchinfo?code=' + line.strip() 
-    r   = requests.get(url)
-    js  =  r.json()
-    print js['data'][0]['price']
