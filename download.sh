0755c65a2864fea8c5d31bbd53ce48668d0e7ea2
runrunliuliu <liuliu.tju@gmail.com>
2017-02-17 18:22:03 +0800

    add kline fetch

--- download.sh
+++ download.sh
@@ -12,7 +12,15 @@ python download.py $day0
 rsync -uhavzm --stats --progress himalayas@139.129.99.51:/home/himalayas/apps2/qts/pytrade2/data/dayk.tar.gz .
 tar -zxvf dayk.tar.gz
 
+# scp multiple period data
+mkdir -p data/tmp/
+rsync -uhavzm --stats --progress himalayas@192.168.200.20:/home/himalayas/kline/kline.tar.gz data/tmp/
+tar -zxvf data/tmp/kline.tar.gz -C data/tmp/
+rm -f data/tmp/kline.tar.gz
 
-scp himalayas@139.129.99.51:/home/himalayas/apps2/qts/pytrade2/data/15mink/* ./data/15mink/
-scp himalayas@139.129.99.51:/home/himalayas/apps/web_dev/cron/app/stock_minning/output/test2.txt ./output/fts/
+python pp_utils.py
+cp -r data/output/* data/
+
+# scp himalayas@139.129.99.51:/home/himalayas/apps2/qts/pytrade2/data/15mink/* ./data/15mink/
+# scp himalayas@139.129.99.51:/home/himalayas/apps/web_dev/cron/app/stock_minning/output/test2.txt ./output/fts/
 scp himalayas@139.129.99.51:/home/himalayas/apps2/qts/pytrade2/data/stockinfo.csv ./data/
