1f837abce5e318349270a96b8f335a4fecfc4338
runrunliuliu <liuliu.tju@gmail.com>
2017-02-21 18:52:21 +0800

    add multiperiod compute in crontab shell

--- zcron.sh
+++ zcron.sh
@@ -25,7 +25,11 @@ rsync -uhavzm --stats --progress himalayas@139.129.99.51:/home/himalayas/apps2/q
 $py indicator.py
 
 ###--- TRAIN ---
-$py pp_selstock.py -m 'train' -s 2016-08-01 -d $day0 -t triangle -p day
+$py pp_selstock.py -m 'train' -s 2017-01-01 -d $day0 -t triangle -p day
+$py pp_selstock.py -m 'train' -s 2017-01-01 -d $day0 -t triangle -p week
+$py pp_selstock.py -m 'train' -s 2017-01-01 -d $day0 -t triangle -p month
+$py pp_selstock.py -m 'train' -s 2017-01-01 -d $day0 -t triangle -p 60min
+$py pp_selstock.py -m 'train' -s 2017-01-01 -d $day0 -t triangle -p 30min
 
 nohup $py pp_selstock.py -m 'stock' -s 2016-08-01 -d $day0 -t triangle 1>logs/$day0.triangle.log 2>&1 &
 nohup $py pp_selstock.py -m 'stock' -s 2016-08-01 -d $day0 -t QUSHI 1>logs/$day0.qushi.log 2>&1 &
