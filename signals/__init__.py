e3f05f310aa5d467c9ddfb6261e8102ea3950372
runrunliuliu <liuliu.tju@gmail.com>
2017-02-27 18:19:37 +0800

    change load method

--- signals/__init__.py
+++ signals/__init__.py
@@ -1,4 +1,5 @@
 from utils.utils import FileUtils
+from collections import OrderedDict
 import os
 import abc
 import json
@@ -33,6 +34,23 @@ class XTsignal(object):
             arr = line.strip().split(',')
             self.__codename[arr[1]] = arr[2]
 
+        self.__dtzq  = OrderedDict() 
+        self.__zqdt  = OrderedDict() 
+        zq = 0
+        for line in open(dirs + '/dayk/ZS000001.csv'):
+            arr = line.strip().split(',')
+            dt  = arr[0]
+
+            self.__dtzq[dt] = zq
+            self.__zqdt[zq] = dt
+
+            zq = zq + 1
+
+    def getPrevDay(self):
+        zq   = self.__dtzq[self.__nday]
+        pday = self.__zqdt[zq - 1]
+        return pday
+
     # load Data
     def loadJSON(self, period):
         ret = dict()
@@ -49,16 +67,23 @@ class XTsignal(object):
                     ret[c] = json.loads(arr[1])
         return ret
 
-    def loadSignals(self):
-        self.__nmonk  = self.loadJSON('monk')
-        self.__nweek  = self.loadJSON('week')
-        self.__ndayk  = self.loadJSON('dayk')
-        self.__n60min = self.loadJSON('60mink')
-        self.__n30min = self.loadJSON('30mink')
+    def loadSignals(self, period='ALL'):
+        if period == 'ALL':
+            self.__nmonk  = self.loadJSON('monk')
+            self.__nweek  = self.loadJSON('week')
+            self.__ndayk  = self.loadJSON('dayk')
+            self.__n60min = self.loadJSON('60mink')
+            self.__n30min = self.loadJSON('30mink')
+
+        if period == 'dayk':
+            self.__ndayk  = self.loadJSON('dayk')
 
     def getSignals(self):
         return (self.__nmonk, self.__nweek, self.__ndayk, \
                 self.__n60min, self.__n30min)
+
+    def getDay(self):
+        return self.__nday
 
     def getCodes(self):
         return self.__codes
