9a13eac7ecfea17fa7468eea1c886b00644f2fac
runrunliuliu <liuliu.tju@gmail.com>
2017-03-10 16:27:50 +0800

    add ZHOUQI and KLINE select for xingtai selection

--- xt_selstock.py
+++ xt_selstock.py
@@ -3,12 +3,19 @@ import sys
 import getopt
 from signals.multiperiod import MultiPeriod 
 from signals.szl import SZL
+from signals.kline import KLINE
+from signals.zq import ZHOUQI
+
+
+def select(dirs, nday):
+    pass
 
 
 def main(argv):
     nday = ''
+    code = 'ZS000001'
     try:
-        opts, args = getopt.getopt(argv,"h:d:s:m:t:",["day="])
+        opts, args = getopt.getopt(argv,"h:d:c:m:t:",["day="])
     except getopt.GetoptError:
         print 'test.py -d <time>'
         sys.exit(2)
@@ -18,14 +25,27 @@ def main(argv):
             sys.exit()
         elif opt in ("-d", "--day"):
             nday = arg
+        elif opt in ("-c", "--code"):
+            code = arg
     dirs = './data/'
 
-    output = './output/select/xt/mp/'
-    sel = MultiPeriod(dirs, nday)
-    sel.select(output)
+    # output = './output/select/xt/mp/'
+    # sel = MultiPeriod(dirs, nday)
+    # sel.select(output)
 
-    output = './output/xt/mp/'
-    sel.dumpFT(output)
+    # output = './output/xt/mp/'
+    # sel.dumpFT(output)
+
+    sel = KLINE(dirs, nday)
+    sel.select('NZX')
+
+    sel = ZHOUQI(dirs, nday)
+    pairs = [(1, 4, 200), (2, 4, 100), (3, 4, 100), (4, 3, 500)]
+    for p in pairs:
+        arr = sel.select(p[0], p[1], p[2])
+        for sl in arr:
+            print 'T+' + str(p[0]), sl[0], sl[1], sl[2]
+    sel.get(code)
 
     output = './output/select/xt/szl/'
     sel = SZL(dirs, nday)
