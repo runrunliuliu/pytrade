84a8b1598dbd60ed25354888c24fcb22b2d7bf5e
runrunliuliu <liuliu.tju@gmail.com>
2017-03-10 16:27:00 +0800

    add redis interface

--- signals/szl.py
+++ signals/szl.py
@@ -1,6 +1,7 @@
 import signals
 import os
 from signals import XTsignal
+from signals import zq 
 
 
 class SZL(XTsignal):
@@ -8,24 +9,23 @@ class SZL(XTsignal):
     def __init__(self, dirs, nday):
         super(SZL, self).__init__(dirs, nday)
         self.loadSignals('dayk')
-
         self.__codes = self.getCodes()
-        self.__sigs  = self.getSignals()
 
+        # Init ZhouQi
+        self.__zq = zq.ZHOUQI(dirs, nday)
+
+    # write Data
     def select(self, dirs):
-        # write Data
         if not os.path.exists(dirs):
             os.makedirs(dirs)
         output = dirs + '/' + self.getDay() + '.csv'
         f = open(output, 'w')
-
-        dayk = self.__sigs[2]
         for c in self.__codes:
-            if c not in dayk:
-                continue
-            szl  = dayk[c]['qs']['szl']
-            ohlc = dayk[c]['qs']['ohlc']
-            if len(szl) > 0 and szl['dir'] == 1:
+            p1 = ['qs', 'szl']
+            szl =  self.getNday(c, p1)
+            p2 = ['qs', 'ohlc']
+            ohlc = self.getNday(c, p2)
+            if szl is not None and len(szl) > 0 and szl['dir'] == 1:
                 np = szl['np']
                 cl = ohlc[3] - np[0]
                 lw = ohlc[2] - np[0]
