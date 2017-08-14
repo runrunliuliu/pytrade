d4d3b9a728db1b5de5965e1f1dbdde1d960e185b
runrunliuliu <liuliu.tju@gmail.com>
2017-08-14 18:25:47 +0800

    fix bug

--- ml/merge.py
+++ ml/merge.py
@@ -6,9 +6,7 @@ import re
 
 class Merge(object):
 
-    def __init__(self, inst, base, output, ts):
-
-        self.__ts = ts 
+    def __init__(self, inst, base, output):
         self.__basedir = base
         self.__header  = ''
 
@@ -28,7 +26,7 @@ class Merge(object):
 
         self.__dtzq = dict()
         self.__dind = []
-       
+
         self.__ft2hd_d = dict()
         for line in open('./conf/header.conf'):
             nline = line.strip()
@@ -40,7 +38,7 @@ class Merge(object):
 
     def loadBaseDayK(self, dpath):
         linenum = 0
-        dzq = 0 
+        dzq = 0
         for line in open(self.__basedir + '/' + dpath):
             if linenum == 0:
                 linenum = linenum + 1
@@ -54,8 +52,8 @@ class Merge(object):
 
             self.__op_d[day] = op
             self.__hi_d[day] = hi
-            self.__lw_d[day] = lw 
-            self.__cl_d[day] = cl 
+            self.__lw_d[day] = lw
+            self.__cl_d[day] = cl
 
             dzq = dzq + 1
             self.__dtzq[day] = dzq
@@ -84,21 +82,21 @@ class Merge(object):
             tmp = []
             if pday is not None:
                 if pday == day:
-                    tmp = self.__min15k[day] 
+                    tmp = self.__min15k[day]
                 tmp.append(tup)
             else:
                 tmp = [tup]
-            self.__min15k[day] = tmp 
-            pday    = day 
+            self.__min15k[day] = tmp
+            pday    = day
             linenum = linenum + 1
 
     def parseMin15k(self):
         head = 'kdj1,kdj2,macd1,macd2,' + \
-            'macd3,macd4,macd5,macd6,macd7,macd8' 
+            'macd3,macd4,macd5,macd6,macd7,macd8'
 
         for k,v in self.__min15k.iteritems():
-            cnt     = 1 
-            kdj     = '0,0' 
+            cnt     = 1
+            kdj     = '0,0'
             macd    = '1024201,0'
             # macd  = '0,0'
             kdj_do  = 0
@@ -120,7 +118,7 @@ class Merge(object):
                 cnt = cnt + 1
             self.__ft15k[k] = kdj + ',' + macd + ',' + macd2
 
-        self.__header = head 
+        self.__header = head
 
     def loadDayk(self, dpath):
         def int2bit(val, cnt):
@@ -147,12 +145,12 @@ class Merge(object):
 
             sxy_bit = ','.join(int2bit(sxy, 6))
             zdf_bit = ','.join(int2bit(zdf, 7))
-            
+
             td  = '0,0,0'
             td1 = int(arr[5])
             if td1 != 1024:
                 td = str(td1) + ',' + str(arr[6]) + ',' + str(arr[7])
-                
+
             self.__ftdayk[day] = sxy_bit + ',' + zdf_bit + ',' + td
 
         self.__header = self.__header + ',' + header
@@ -174,7 +172,7 @@ class Merge(object):
     # 前向非累积受益
     def getFOneDayRet(self, day):
         ret  = []
-        fret = 3 
+        fret = 3
         nind = self.__dtzq[day]
         for i in range(0, fret):
             if (nind + 1) >= len(self.__dind):
@@ -182,7 +180,7 @@ class Merge(object):
             else:
                 ncl = self.__cl_d[self.__dind[nind - 1]]
                 fcl = self.__cl_d[self.__dind[nind]]
-                r = (fcl - ncl) / ncl 
+                r = (fcl - ncl) / ncl
                 r = "{:.4f}".format(r)
                 ret.append(r)
             nind = nind + 1
@@ -196,9 +194,9 @@ class Merge(object):
             if (nind + i) < 0:
                 ret.append('NULL')
             else:
-                yop = self.__cl_d[self.__dind[nind + i - 1]] 
+                yop = self.__cl_d[self.__dind[nind + i - 1]]
                 ncl = self.__cl_d[day]
-                r = (ncl - yop ) / yop 
+                r = (ncl - yop ) / yop
                 r = "{:.4f}".format(r)
                 ret.append(r)
         return ret
@@ -208,7 +206,7 @@ class Merge(object):
         cnt     = 0
         header  = ''
         ft3dict = dict()
-        ftleng  = 0
+        ftlen   = 0
         for line in open(self.__output + '/' + fname):
             if cnt == 0:
                 header = line.strip()[4:]
@@ -224,9 +222,9 @@ class Merge(object):
                     else:
                         fts.append(arr[i])
                 ft3dict[sday] = fts
-                ftleng = len(fts)
+                ftlen = len(fts)
             cnt = cnt + 1
-        return(header, ft3dict, ftleng)
+        return(header, ft3dict, ftlen)
 
     # Feature from LW
     def loadFT2(self, fname):
@@ -251,7 +249,7 @@ class Merge(object):
                         fts.append('1024201')
                     else:
                         fts.append(arr[i])
-                
+
                 # Feature Handles
                 for k in range(0, len(fts)):
                     if k == 19 and int(fts[18]) <= 2:
@@ -268,33 +266,35 @@ class Merge(object):
         # Get Header Index
         hd_index = dict()
         for line in open(path):
-            arr = line.strip().split(',')
+            arr = line.strip().split('\t')
             for i in range(0, len(arr)):
                 hd_index[i] = arr[i]
             break
-        cnt  = 0
+        cnt     = 0
         headers = []
+
+        cxbd    = 1
+        indexes = []
         for line in open(path):
-            arr = line.strip().split(',')
+            arr = line.strip().split('\t')
             fts = []
+
+            if cnt > 0:
+                indexes.append(float(arr[1]))
+
+            if len(indexes) > 3:
+                cxbd = (indexes[-1] - indexes[-4]) / indexes[-4]
+
             for i in range(0, len(arr)):
                 if hd_index[i] not in self.__ft2hd_d:
                     continue
                 if cnt == 0:
                     headers.append(self.__ft2hd_d[arr[i]])
                 else:
-                    # special handling 急涨急跌
-                    # desname = self.__ft2hd_d[hd_index[i]]
-                    # if desname == 'jjdmin' and self.__ts.comparedt(arr[0], '20160301', '%Y%m%d'):
-                    #     arr[i] = ''
-
                     if len(arr[i]) == 0:
-                        # special handling
-                        if i == len(arr) - 1:
-                            continue
                         arr[i] = '1024201'
                     else:
-                        if hd_index[i].decode('utf8') == u'上证涨跌幅' and float(arr[i]) <= -0.01:
+                        if hd_index[i].decode('utf8') == u'上证涨跌幅' and (float(arr[i]) <= -0.01 or cxbd <= -0.01):
                             fday.write(arr[0] + ',' + arr[i] + '\n')
                     fts.append(arr[i])
             if cnt == 0:
@@ -339,8 +339,8 @@ class Merge(object):
             # GET Day Index
             fret1 = ','.join(self.getFRet(k))
             fret2 = ','.join(self.getFOneDayRet(k))
-            f.write(k + ',' + ft15k + ',' + v + ',' + ft2flat + ',' + fret1 + ',' + fret2) 
-            # f.write(k + ',' + ft15k + ',' + v + ',' + fret1 + ',' + fret2) 
+            f.write(k + ',' + ft15k + ',' + v + ',' + ft2flat + ',' + fret1 + ',' + fret2)
+            # f.write(k + ',' + ft15k + ',' + v + ',' + fret1 + ',' + fret2)
             f.write('\n')
         f.close()
 #
