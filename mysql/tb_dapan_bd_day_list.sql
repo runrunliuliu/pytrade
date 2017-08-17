e58ebaa94740c82ac07d7f8237a2a414601d2eb6
runrunliuliu <liuliu.tju@gmail.com>
2017-08-17 17:39:26 +0800

    update dapan_bd_day list

--- mysql/tb_dapan_bd_day_list.sql
+++ mysql/tb_dapan_bd_day_list.sql
@@ -1,11 +1,12 @@
 DROP TABLE IF EXISTS `tb_dapan_bd_day_list`;
 CREATE TABLE `tb_dapan_bd_day_list` (  
     `id` int(11) NOT NULL AUTO_INCREMENT,  
-    `day`  varchar(12),  
-    `status` int(11) DEFAULT '0',  
-    `predict` int(11) NOT NULL,  
-    `prob` float(11) NOT NULL,  
-    `pday` varchar(20) NOT NULL,  
+    `day`  int(11) NOT NULL DEFAULT 0,  
+    `status` int(11) DEFAULT 0,  
+    `predict` int(11) NOT NULL DEFAULT 0,  
+    `prob` float(11) NOT NULL DEFAULT 0,  
+    `pday` varchar(20) NOT NULL DEFAULT '',  
+    `uptime` int(11) NOT NULL DEFAULT 0,  
     INDEX(`day`),
     PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
